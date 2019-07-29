9fc50c98cdc02a39e36dd6b8f8e1e5170f5ad665
Ben Scott <bengscott2@gmail.com>
2019-07-29 11:46:40 +0100

    refactor: tests will create new instance of bank prior to each it block

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,15 +1,19 @@
 require 'bank'
 
 describe Bank do
+  before(:all) do
+    @bank = Bank.new
+  end
+
   it 'has an account balance that is initially set at zero' do
     expect(subject.get_balance).to eq 0
   end
 
   describe '#deposit' do
     it 'will add to account balance amount passed to deposit' do
-      expect(subject.get_balance).to eq 0
-      subject.deposit(100)
-      expect(subject.get_balance).to eq 100
+      expect(@bank.get_balance).to eq 0
+      @bank.deposit(100)
+      expect(@bank.get_balance).to eq 100
     end
   end
 end
